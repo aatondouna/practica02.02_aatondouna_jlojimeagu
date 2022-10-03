e6bf9ddfd2ed021b4a5be4a7d20152e746a9f098
aatondouna <aatondouna1@educacion.navarra.es>
2022-10-03 12:05:10 +0200

    Ejercicio9 completado

---
+++ Ejercicio9.py
@@ -0,0 +1,10 @@
+cantidad_invertir = int(input("cantidad a invertir\n"))
+
+interes_anual = int(input("interes anual\n"))
+
+numero_años = int(input("numero de años\n"))
+
+x = cantidad_invertir + (cantidad_invertir * interes_anual * numero_años)
+
+print("capital obtenido en la inversion", x)
+

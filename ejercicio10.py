ae229214d5a931a6b63ca4bb5049c37c27494ff3
juan miguel <jlojimeagu1@educacion.navarra.es>
2022-10-03 12:04:03 +0200

    ejercicio10 completado

---
+++ ejercicio10.py
@@ -0,0 +1,6 @@
+payaso = 112
+muneca = 75
+n_payasos = int(input('cuantos payasos='))
+n_munecas = int(input('cuantas muñecas='))
+print('peso de los payasos', n_payasos*payaso, 'g')
+print('peso de las muñecas', n_munecas*muneca, 'g')

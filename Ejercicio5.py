00c590e4ff911a1c82eb01803e3d7f13f7eba582
aatondouna <aatondouna1@educacion.navarra.es>
2022-10-03 11:35:36 +0200

    Ejercicio5 completado

---
+++ Ejercicio5.py
@@ -0,0 +1,10 @@
+num_horas = int(input("introduce el numero de horas trabajadas\n"))
+
+coste_hora = int(input("introduce el coste por hora\n"))
+
+paga = num_horas * coste_hora
+print("te corresponde", paga, "€")
+
+
+
+

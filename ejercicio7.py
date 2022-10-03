765751f35ab398f4fbb24e6a58a9a616ff5a18ed
aatondouna <aatondouna1@educacion.navarra.es>
2022-10-03 11:48:23 +0200

    Ejercicio7 completado

---
+++ ejercicio7.py
@@ -0,0 +1,13 @@
+peso = int(input("Introduzca su peso en Kg\n"))
+
+estatura = int(input("introduzca su estatura en metros\n"))
+
+x = peso/estatura
+
+round(x, 2)
+
+print("tu índice de masa corporal es", x)
+
+
+
+
